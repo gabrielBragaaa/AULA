64b3aa98373d62a95a8ec05cb3ca16f26b692914
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-07-29 15:50:50 -0300

    Impar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,35 +7,15 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int numCli = 0;
-        int alcool = 0;
-        int gasolina = 0;
-        int diesel = 0;
+        System.out.println("Insira o valor Inteiro : ");
+       int impar = sc.nextInt();
+        System.out.println("Insira o valor minimo: ");
+        for (int x = sc.nextInt(); x <= impar; x++){
+            if (x % 2 != 0) {
+                System.out.println(x);
 
-
-        System.out.println(" 1.Alcool \n 2.Gasolina \n 3.Diesel \n 4.Fim");
-
-        while (numCli != 4) {
-
-            System.out.println("Tipo de Abastecimento: ");
-            numCli = sc.nextInt();
-
-            if (numCli == 1) {
-                alcool += 1;
-            } else if (numCli == 2) {
-                gasolina += 1;
-            } else if (numCli == 3) {
-                diesel += 1;
-
-            } else {
-                System.out.println("CODIGO INVALIDO :(");
             }
 
         }
-        System.out.println("Alcool : " + alcool);
-        System.out.println("Gasolina : " + gasolina);
-        System.out.println("Diesel : " + diesel);
-        System.out.println("MUITO OBRIGADO :)");
-        sc.close();
     }
 }
--- src/main/java/org/example/PostoDeGasolina.java
+++ src/main/java/org/example/PostoDeGasolina.java
@@ -6,29 +6,30 @@ public class PostoDeGasolina {
 //    Scanner sc = new Scanner(System.in);
 //
 //    int numCli = 0;
-//    int alcool = 0;
-//    int gasolina = 0;
-//    int diesel = 0;
+//        int alcool = 0;
+//        int gasolina = 0;
+//        int diesel = 0;
 //
-//        System.out.printl(" 1.Alcool \n 2.Gasolina \n 3.Diesel \n 4.Fim");
+//
+//        System.out.println(" 1.Alcool \n 2.Gasolina \n 3.Diesel \n 4.Fim");
 //
 //        while (numCli != 4) {
 //
-//        System.out.println("Tipo de Abastecimento: ");
-//        numCli = sc.nextInt();
+//            System.out.println("Tipo de Abastecimento: ");
+//            numCli = sc.nextInt();
 //
-//        if (numCli == 1) {
-//            alcool += 1;
-//        } else if (numCli == 2) {
-//            gasolina += 1;
-//        } else if (numCli == 3) {
-//            diesel += 1;
+//            if (numCli == 1) {
+//                alcool += 1;
+//            } else if (numCli == 2) {
+//                gasolina += 1;
+//            } else if (numCli == 3) {
+//                diesel += 1;
 //
-//        } else {
-//            System.out.println("CODIGO INVALIDO :(");
+//            } else {
+//                System.out.println("CODIGO INVALIDO :(");
+//            }
+//
 //        }
-//
-//    }
 //        System.out.println("Alcool : " + alcool);
 //        System.out.println("Gasolina : " + gasolina);
 //        System.out.println("Diesel : " + diesel);
