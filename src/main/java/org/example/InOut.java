c5d5176d507082b8ddc91a8b3d90acdc809b230d
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-07-30 15:31:22 -0300

    InOut

---
+++ src/main/java/org/example/InOut.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class InOut {
+//
+//    int n = sc.nextInt();
+//    int interValor = 0;
+//    int in = 0;
+//    int out = 0;
+//        for (int x = 1 ; x <= n; x++){
+//        interValor = sc.nextInt();
+//        if (interValor >=10 && interValor<= 20){
+//            in +=1;
+//        }else{
+//            out += 1;
+//        }
+//
+//    }
+//        System.out.println(in + " in");
+//        System.out.println(out + " out");
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,23 +4,5 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        int interValor = 0;
-        int in = 0;
-        int out = 0;
-        for (int x = 1 ; x <= n; x++){
-           interValor = sc.nextInt();
-            if (interValor >=10 && interValor<= 20){
-               in +=1;
-            }else{
-                out += 1;
-            }
-
-        }
-        System.out.println(in + " in");
-        System.out.println(out + " out");
     }
 }
