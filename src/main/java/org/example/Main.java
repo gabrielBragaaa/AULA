14864287af62ab98d66470f16dceb712e97a1a8d
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-07-30 17:52:33 -0300

    Media

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,25 @@
 package org.example;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int valorTeiro = sc.nextInt();
+        double caso1, caso2, caso3 = 0;
+        double media = 0;
+        double total = 0;
+
+        for (int x = 1; x <= valorTeiro; x++) {
+            caso1 = sc.nextDouble();
+            media =caso1 * 2 + caso1 * 3 + caso1 * 5;
+            total = media / 10;
+        }
+        System.out.println(total);
+        sc.close();//pesquisar depois
     }
 }
