219303c93368c1d376b71d856e8d1975e8ad0067
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-08-01 15:37:58 -0300

    New

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,21 +8,6 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-
-        double total,num1 ,num2 = 0;
-
-        for(int x = 0 ; x < n; x++){
-            num1 = sc.nextDouble();
-            num2 = sc.nextDouble();
-            total = num1 / num2;
-            if(num2 == 0){
-                System.out.println("Divisão impossivel");
-            }else{
-                System.out.println(total);
-            }
-
-        }
 
     }
 }
---
+++ src/main/java/org/example/denominador.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class denominador {
+//    int n = sc.nextInt();
+//
+//    double total,num1 ,num2 = 0;
+//
+//        for(int x = 0 ; x < n; x++){
+//        num1 = sc.nextDouble();
+//        num2 = sc.nextDouble();
+//        total = num1 / num2;
+//        if(num2  == 0){
+//            System.out.println("Divisão impossivel");
+//        }else{
+//            System.out.println(total);
+//        }
+//
+//    }
+
+}
