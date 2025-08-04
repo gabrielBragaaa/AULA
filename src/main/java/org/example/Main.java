9cc399d7ae5ff91d810e4e9f2cca680d2bce8bd7
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-08-04 09:26:00 -0300

    Divisores

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,12 +11,10 @@ public class Main {
 
         int n = sc.nextInt();
 
-        int fatorial = 1;
-
         for (int x = 1; x <= n; x++) {
-            fatorial *= x;// este valor ira receber o este resultado durante N vezes.
+            if (n % x == 0) {
+                System.out.println(x);
+            }
         }
-        System.out.println(fatorial);
-        sc.close();
     }
 }
