2dc8740ab779fb593eba7bc39148ff8a17947030
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-08-08 17:51:39 -0300

    Celsius em Fireheint

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.sql.SQLOutput;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,11 +10,19 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("diGITE A TEMPERATURA EM CELSIUS: ");
-        double c = sc.nextDouble();
-        double f = 9.0 * c / 5.0 + 32.0;
-        System.out.printf("EQUIVALENTE EM FAHRENHEIT: %.1f%n ", f);
+        char resp;
 
+        do {
+            System.out.print("DIGITE A TEMPERATURA EM CELSIUS: ");
+            double c = sc.nextDouble();
+            double f = 9.0 * c / 5.0 + 32.0;
+            System.out.printf("EQUIVALENTE EM FAHRENHEIT: %.1f%n ", f);
+            System.out.print("VOÇE QUER CONTINUAR (S / N) ? ");
+            resp = sc.next().charAt(0);
+        } while (resp != 'n' && resp != 'N');
+
+        System.out.println();
+        System.out.println("ATÉ LOGO :)");
         sc.close();
     }
 }    
