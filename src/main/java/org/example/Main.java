2060e21342c1dc6ee47e86802fea9f6f5bc67b66
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-08-26 10:26:31 -0300

    Minusculo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,21 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
+        String original = "abcd EFGHIJ ABC abc DEFG";
 
-        int mask = 0b100000;
-        int n = sc.nextInt();
+        //Minusculo
+        String s01 = original.toLowerCase();
+        System.out.println("Original: -" + original + " - ");
+        System.out.println("Original: -" + s01 + " -");
 
-        if ((n & mask) != 0){
-            System.out.println("6th bit is true!");
-        }else {
-            System.out.println("6th bit is false!");
-        }
-
-
-
-
-
-
-}
+    }
 }    
