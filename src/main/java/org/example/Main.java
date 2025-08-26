b7c39b26bc24206eb6325bf581b534eaa1e31cdd
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-08-26 10:51:10 -0300

    Maiusculo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String original = "abcd EFGHIJ ABC abc DEFG";
+        String original = "abcd EFGHIJ ABC abc DEFG  ";
 
         //Minusculo
         String s01 = original.toLowerCase();
+        String s02 = original.toUpperCase();
+        String s03 = original.trim();
         System.out.println("Original: -" + original + " - ");
-        System.out.println("Original: -" + s01 + " -");
+        System.out.println("ToLowerCase: -" + s01 + " -");
+        System.out.println("ToUpperCase: -" + s02 + " -");
+       
+
 
     }
 }    
