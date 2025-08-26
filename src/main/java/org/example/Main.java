f6560ffb7763eff7cc28f9ad3f61ab8ac8a0a067
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-08-26 10:55:24 -0300

    Substring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,10 +11,13 @@ public class Main {
         String s01 = original.toLowerCase();
         String s02 = original.toUpperCase();
         String s03 = original.trim();
+        String s04 = original.substring(1);
+
         System.out.println("Original: -" + original + " - ");
         System.out.println("ToLowerCase: -" + s01 + " -");
         System.out.println("ToUpperCase: -" + s02 + " -");
         System.out.println("Trim -"+ s03 + "-");
+        System.out.println("Substring -" + s04 + "-");
 
 
     }
