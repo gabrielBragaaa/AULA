c8c6aa1bb2dbaf888f4a410d903298def6d72021
gabrielBragaaa <gabrielzinhobraga04@gmail.com>
2025-08-26 11:36:32 -0300

    Funções String

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,29 +5,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String original = "abcd EFGHIJ ABC abc DEFG  ";
 
-        //Minusculo
-        String s01 = original.toLowerCase();
-        String s02 = original.toUpperCase();
-        String s03 = original.trim();
-        String s04 = original.substring(1);
-        String s05 = original.substring(1, 9);
-        String s06 = original.replace("a", "W");
-        String s07 = original.replace("abc", "WZX");
-        int i = original.indexOf("bc");
-        int l = original.lastIndexOf("bc");
-
-        System.out.println("Original: -" + original + " - ");
-        System.out.println("ToLowerCase: -" + s01 + " -");
-        System.out.println("ToUpperCase: -" + s02 + " -");
-        System.out.println("Trim -" + s03 + "-");
-        System.out.println("Substring -" + s04 + "-");
-        System.out.println("Substring -" + s05 + "-");
-        System.out.println("Replace -" + s06 + "-");
-        System.out.println("Replace -" + s07 + "-");
-        System.out.println("IndexOff -"+ i + "-");
-        System.out.println("LastOff -"+ l + "-");
 
 
     }
